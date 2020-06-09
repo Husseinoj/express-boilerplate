3f7e4b00d73998f1af93cd2d82604fb86a47f3b1
Hussein Ojaghi <h.ojaghi@roundtableapps.com>
2020-06-09 17:51:39 +0430

    remove useless routes

--- config/router.js
+++ config/router.js
@@ -2,20 +2,12 @@ import express from 'express';
 
 // import controllers
 import {signIn, signUp} from '../app/controllers/profile.controller';
-// import user from '../app/controllers/profile.controller';
 
 const router= express.Router();
 
-// Auths
+// Profile
 router.post('/auth/signup', signUp())
 router.post('/auth/singin', signIn())
-
-// Users
-// router.get('/users',user.index())
-// router.get('/user/:id',user.show())
-// router.put('/user/:id',user.update())
-// router.delete('/user/:id',user.delete())
-// router.post('/user',user.create())
 
 
 export default router;
