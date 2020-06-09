6545811e7ea16749dbebc9b93fedda6d43657cc5
Hussein Ojaghi <h.ojaghi@roundtableapps.com>
2020-06-09 14:14:13 +0430

    add body parser to parse json and application/x-www-form-urlencoded

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+import router from '../config/router';
 
 const app = express();
 const port = 30000;
-app.get("*", (req, res) => {
-    res.status(200).json({
-        message:"server is ok"
-    })
-})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(router);
+
 app.listen(port, ()=> console.log(`server running on http://localhost:${port}`));
