19f359437b62574a078d3c8103481730a671730b
Hussein Ojaghi <h.ojaghi@roundtableapps.com>
2020-06-13 16:01:28 +0430

    clean the code and remove useless spaces

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 
-const databaseUrl= process.env.DATABASE_URL
-const databasePort= process.env.DATABASE_PORT;
+const databaseUrl  = process.env.DATABASE_URL
+const databasePort = process.env.DATABASE_PORT;
 const databaseName = process.env.DATABASE_NAME;
 
 mongoose.connect(`mongodb://${databaseUrl}:${databasePort}/${databaseName}`)
