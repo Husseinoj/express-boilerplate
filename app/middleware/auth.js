25042ac78ee2299e9e4df6fb04cc10e017fbc56f
Hussein Ojaghi <h.ojaghi@roundtableapps.com>
2020-06-13 15:43:04 +0430

    implement resource and test it on admin entity

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -6,17 +6,20 @@ dotenv.config();
 const privateKey=  process.env.PRIVATE_KEY;
 
 export default async (req, res, next) => {
-        try {
-                const token = req.headers.token.split(' ')[1];
-                if(!token)
-                return res.status(500).json({message:'token must be provided'});
+   try {
+         let token = '';
 
-                const decoded = await jwt.verify(token, privateKey);
-                req.user = decoded.user;
-                next();
-        }
-        catch(error){
-                res.status(500).json({message: error.message})
-        }
+         if(req.headers.token)
+             token= req.headers.token.split(' ')[1];
+                    
+         if(!token)
+            return res.status(500).json({message:'token must be provided'});
+
+        const decoded = await jwt.verify(token, privateKey);
+        req.user = decoded.user;
+        next();
+   } catch(error) {
+        res.status(500).json({message: error.message})
+   }
       
 }
